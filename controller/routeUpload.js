4cd778fc03c0f9fc22879106028cfa7e35d9a918
Yanya Singh <yanyapr@gmail.com>
2024-08-14 17:07:31 +0530

    Adding routeUpload.js

--- controller/routeUpload.js
+++ controller/routeUpload.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const cloudinary = require('../utils/cloudinary');
 const upload = require('../middleware/multer');
+const axios = require('axios');
+const recognizeText = require('../test_extractor');
 
-router.post('/upload', upload.single('textImage'), function(req, res){
+
+router.post('/upload', upload.single('textImage'), async function(req, res){
     
     // if (!req.file) {
     //     return res.status(400).send('No file uploaded.');
@@ -11,23 +14,39 @@ router.post('/upload', upload.single('textImage'), function(req, res){
 
     // const filePath = req.file.path;
 
-    cloudinary.uploader.upload(req.file.path, function(err, result){
-        if(err){
-            console.log(err);
-            return res.status(500).json({
-                success: false,
-                message: "Error uploading to Cloudinary"
-            });
-        }
+    try {
+        // upload image to cloudinary
+        const result =  await cloudinary.uploader.upload(req.file.path);
 
-        // const image_url = result.secure_url;
+        // get image url from the cloudinary response
+        const imageUrl = result.secure_url;
 
+        // fetch the image data from cloudinary as a buffer
+        const response = await axios.get(imageUrl, {responseType:'arraybuffer' });
+        const imageBuffer = Buffer.from(response.data, 'binary');
+
+        // Extract text from the image buffer using recongizeText function
+        const extractedText = await recognizeText(imageBuffer);
+
+        // send response with image URL and extracted text
         res.status(200).json({
             success: true,
-            message: "Uploaded!",
-            data: result
+            message: "Uploaded and Text Extracted!",
+            data: {
+                imageUrl: imageUrl,
+                extractedText: extractedText
+            }
         });
-    });
+         
+    }catch(err){
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: "Error processing the image",
+            error: err.message
+        });
+    }
+
 });
 
 module.exports = router;

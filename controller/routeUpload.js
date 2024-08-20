228c8cf54261f23ec2e3b69f8a554040febf4fac
Yanya Singh <yanyapr@gmail.com>
2024-08-20 16:07:14 +0530

    Removed uncessary comments

--- controller/routeUpload.js
+++ controller/routeUpload.js
@@ -3,30 +3,23 @@ const router = express.Router();
 const cloudinary = require('../utils/cloudinary');
 const upload = require('../middleware/multer');
 const axios = require('axios');
-// const recognizeText = require('../core/text_extractor');
 const { deleteImageCheck } = require('../utils/counterUtils')
 
 
 router.post('/upload', upload.single('textImage'), async function(req, res){
     
-    // if (!req.file) {
-    //     return res.status(400).send('No file uploaded.');
-    // }
-
-    // const filePath = req.file.path;
-
     try {
 
-        // check for storage limit
+      
         await deleteImageCheck();
 
-        // upload image to cloudinary
+        
         const result =  await cloudinary.uploader.upload(req.file.path);
 
-        // get image url from the cloudinary response
+       
         const imageUrl = result.secure_url;
 
-        // calling the text extraction endpoint
+       
         const textExtractionResponse = await axios.post('http://localhost:8000/api/extract-text', {
             imageUrl: imageUrl
         });      
@@ -41,8 +34,6 @@ router.post('/upload', upload.single('textImage'), async function(req, res){
         // });
 
         
-
-        // send response with image URL and extracted text
         res.status(200).json({
             success: true,
             message: "Uploaded and Text Extracted!",
